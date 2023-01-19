fbea33cc7cc8e0b42a2f1017412e912ca3739f8c
Fahim Faisal <mdfahimfaisal000@gmail.com>
2023-01-19 17:34:59 +0600

    prod issue solved

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.get("/", (req, res) => {
   res.send("Hello from PluggedIn Server");
 });
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.tzinyke.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@pluggedin.lv6yqjw.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
