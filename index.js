2b64e0ed2e4846f09596b2b78d7f4684e611bf8b
Fahim Faisal <mdfahimfaisal000@gmail.com>
2023-01-23 22:35:29 +0600

    delete api

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -52,6 +52,14 @@ async function run() {
       const query = { authorEmail: email };
       const medias = await recordsCollection.find(query).toArray();
       res.send(medias);
+    });
+
+    app.delete("/record/:id", async (req, res) => {
+      const recordId = req.params.id;
+
+      const query = { _id: ObjectId(recordId) };
+      const result = await recordsCollection.deleteOne(query);
+      res.send(result);
     });
   } finally {
   }
