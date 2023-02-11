93eea2bd9fe369181581b784d8c2392e7701dd4a
Fahim Faisal <mdfahimfaisal000@gmail.com>
2023-02-11 09:56:03 +0600

    a

--- index.js
+++ index.js
@@ -33,17 +33,17 @@ const client = new MongoClient(uri, {
 
 function verifyJWT(req, res, next) {
   const authHeader = req.headers.authorization;
-  // if (!authHeader) {
-  //     return res.status(401).send({ message: "Unauthorized Access" });
-  // }
-  //   const token = authHeader.split(" ")[1];
-  //   jwt.verify(token, process.env.ACC_Token, function (err, decoded) {
-  // if (err) {
-  //     return res.status(403).send({ message: "Forbidden Access" });
-  // }
-  // req.decoded = decoded;
-  next();
-  // });
+  if (!authHeader) {
+    return res.status(401).send({ message: "Unauthorized Access" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACC_Token, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden Access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
 }
 
 async function run() {
